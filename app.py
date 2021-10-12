1c4ca037d27688f833759517562eb15122e84c27
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2021-10-12 03:07:32 +0300

    Изменено представление search

--- app.py
+++ app.py
@@ -18,19 +18,20 @@ def paging():
     return render_template("main.html")
 
 
-@app.route('/search')
+@app.route('/search/')
 def search():
-    model = request.args.get('model')
-    with open('entities.json') as f:
-        entities = json.load(f)
-        response = []
-        if not model:
-            response = entities
-        else:
+    model = str(request.args.get('model'))
+    model = model.split(' ')
+    response = []
+    if not model:
+        response = entities
+    else:
+        for i in model:
             for e in entities:
-                if e["model"] == model:
-                    response.append(e)
-        return render_template("search_ause.html", entities=response)
+                for ent in e["model"].split(' '):
+                    if ent == i:
+                        response.append(e)
+    return render_template("search_ause.html", entities=response)
 
 
 @app.route('/card/<int:eid>')
